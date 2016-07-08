6c46b0d946c8fa5a4e83c68d1dffb3c2befa706c
kevinfan23 <kevinfan23@ucla.edu>
2016-07-08 16:16:34 -0700

    update cv

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-	$('.navbar').addClass('animated slideInDown');
+	$('.navbar').addClass('animated fadeInDown');
+	
 	$('.intro-click').on('click', function() {
 		setTimeout(function(){
 			window.location.href='index.html'}, 
 			500
 		);
-	$('.navbar').addClass('animated slideOutUp');
+	$('.navbar').addClass('animated fadeOutUp');
 	});
 	$('.about-click').on('click', function() {
 		setTimeout(function(){
 			window.location.href='about.html'}, 
 			500
 		);
-	$('.navbar').addClass('animated slideOutUp');
+	$('.navbar').addClass('animated fadeOutUp');
 	});
 	$('.resume-click').on('click', function() {
 		setTimeout(function(){
 			window.location.href='resume.html'}, 
 			500
 		);
-	$('.navbar').addClass('animated slideOutUp');
+	$('.navbar').addClass('animated fadeOutUp');
 	});
 });
